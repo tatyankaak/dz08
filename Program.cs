166fb23ed1e10f3ffb561e55c87920a9b4c42c7b
Татьяна <robsten9086@mail.ru>
2023-07-09 16:50:49 +0300

    задача 8

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод числа N
+        Console.Write("Введите число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        // Вывод всех четных чисел от 1 до N
+        Console.WriteLine("Четные числа от 1 до N:");
+        for (int i = 1; i <= N; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
